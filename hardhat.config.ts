178657aa2f764b90dd710eb6c4d02aa0c4954a09
anurag-iitk <iamanuragsaini0@gmail.com>
2024-09-24 13:22:02 +0530

    update: hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import * as path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+const privateKey = process.env.PRIVATE_KEY || "";
+const optimismId = process.env.OPTIMISM_KEY || "";
+const arbitrum_id = process.env.ARBITRUM_KEY || "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -13,6 +20,14 @@ const config: HardhatUserConfig = {
         count: 1000,
       },
     },
+    optimism: {
+      url: `https://opt-sepolia.g.alchemy.com/v2/${optimismId}`,
+      accounts: privateKey ? [privateKey] : [],
+    },
+    arbitrum: {
+      url: `https://arb-sepolia.g.alchemy.com/v2/${arbitrum_id}`,
+      accounts: privateKey ? [privateKey] : [],
+    }
   },
   solidity: {
     version: "0.8.27",
